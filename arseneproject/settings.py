3a1eb6400c6089bd359de1b42b5aee02976c316a
dbib <toddbacc@gmail.com>
2023-11-30 08:49:44 +0200

    ajout de l'appname et des infos de la base des donnees dans installed_apps dans settings.py

--- arseneproject/settings.py
+++ arseneproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'covidapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'arseneproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'covidapp2',
+        'USER': 'arkiev',
+        'PASSWORD': 'congo1960pass',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
